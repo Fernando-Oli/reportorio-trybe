d9915e31e63e29f07d940ea0a862be12fb6617ab
Fernando Oliveira <primoflro@gmail.com>
2022-05-13 15:24:36 -0300

    Bloco 4, exerciiso até 11

---
+++ fundamentos/javascript-dom-eventos-e-web-storage/04-02/academia2.js
@@ -0,0 +1,18 @@
+/*let age = 20
+let sex = "F"
+let weight = 70
+let height = 170
+if(sex == "M"){
+    console.log("A taxa metabólica basal é " + ((height*6.25)+(weight*9.99)-(age*4.92)+5) + "Kcal")
+}
+else if(sex == "F"){
+    console.log("A taxa metabólica basal é " + ((height*6.25)+(weight*9.99)-(age*4.92)-161) + "Kcal")
+}
+*/
+let pizzas = ['perfeito', 'pei', 'fer', 'to', 'daoasd']
+pizzas.push('Peito de Peru')//adiciona ao final
+pizzas.unshift()//adiciona ao inicio
+pizzas.pop()//remove a ultima tarefa
+let index = pizzas.indexOf('perfeito')//usado para procurar dentro da Array
+console.log(index)
+
---
+++ fundamentos/javascript-dom-eventos-e-web-storage/04-02/exercicios-for.js
@@ -0,0 +1,7 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let x = 0; x < groceryList.length; x++){
+    console.log(groceryList[x])
+}
+
+let numeros = [1,2,3,4,5];
