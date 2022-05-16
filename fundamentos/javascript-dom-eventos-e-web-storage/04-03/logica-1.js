ed33c5b5eb87bdf8032fd33bd28808b23cf3d877
Fernando Oliveira <primoflro@gmail.com>
2022-05-16 15:24:36 -0300

    Primeiro bloco 4, dia 3

---
+++ fundamentos/javascript-dom-eventos-e-web-storage/04-03/logica-1.js
@@ -0,0 +1,12 @@
+Pegar um copo
+Colocar o copo em cima da mesa
+Abrir a porta da geladeira
+Pegar o leite
+Fechar a porta da geladeira
+Colocar determinada quantidade de leite no copo
+Abrir o armário
+Pegar o achocolatado
+Fechar a porta do armário
+Colocar determinada quantidade de achocolatado no copo
+Pegar uma colher
+Mexer no copo com a colher
