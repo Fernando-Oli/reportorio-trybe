8a7b203a801db6fa417a7481b208fdb96159f637
Fernando Oliveira <primoflro@gmail.com>
2022-05-18 06:48:02 -0300

    finais

--- fundamentos/javascript-dom-eventos-e-web-storage/04-03/bonus6-logica-programacao.js
+++ fundamentos/javascript-dom-eventos-e-web-storage/04-03/bonus6-logica-programacao.js
@@ -1,12 +1,19 @@
 let num = 4;
+//número a ser avaliado
 let divisores = 0;
+//quantidade de divisores que o num terá
 for(let x = 1; x <=num; x+=1){
+//estrutura que passa entre todos os número entre 1 e o número a ser avaliados, ou seja
+//seus divisores
     if(num%x==0){
+// se o num for dividido for x e o resto for 0, achamos um divisor e ele será adicionado a variável
         divisores +=1;
     }
 }
 if(divisores==2){
+//se o número de dividores for igual a 2, significa que ele é primo, porque só se divide por 1 e ele mesmo.
     console.log(`Esse número ${num} é primo`);
 } else{
-    console.log(`Esse número ${num} é par`);
+//senão não é primo
+    console.log(`Esse número ${num} não é primo`);
 }
