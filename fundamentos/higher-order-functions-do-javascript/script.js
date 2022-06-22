ebc9cedd700d284a780906d12d6ac16c1ce0a330
Fernando Oliveira <primoflro@gmail.com>
2022-06-22 19:50:14 -0300

    Bloco 8 dia 1

---
+++ fundamentos/higher-order-functions-do-javascript/script.js
@@ -0,0 +1,9 @@
+const wakeUp = () => 'Acordando!!';
+
+const makeCoffe = () => 'Bora tomar café!';
+
+const goToSleep = () => 'Partiu dormir!!';
+
+const doingThis = (action) => action();
+
+console.log(doingThis(wakeUp));
--- fundamentos/introducao-javascript-es6-e-testes-unitarios/07-03/funcionario.js
+++ fundamentos/introducao-javascript-es6-e-testes-unitarios/07-03/funcionario.js
@@ -1,67 +1,64 @@
+const { info } = require("console");
+
 // Dados
 const professionalBoard = [
-    {
-      id: '8579-6',
-      firstName: 'Ana',
-      lastName: 'Gates',
-      specialities: ['UX', 'Design'],
-    },
-    {
-      id: '5569-4',
-      firstName: 'George',
-      lastName: 'Jobs',
-      specialities: ['Frontend', 'Redux', 'React', 'CSS'],
-    },
-    {
-      id: '4456-4',
-      firstName: 'Leila',
-      lastName: 'Zuckerberg',
-      specialities: ['Context API', 'RTL', 'Bootstrap'],
-    },
-    {
-      id: '1256-4',
-      firstName: 'Linda',
-      lastName: 'Bezos',
-      specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
-    },
-    {
-      id: '9852-2-2',
-      firstName: 'Jeff',
-      lastName: 'Cook',
-      specialities: ['Ruby', 'SQL'],
-    },
-    {
-      id: '4678-2',
-      firstName: 'Paul',
-      lastName: 'Dodds',
-      specialities: ['Backend'],
-    },
-  ];
-  
-  // Pesquisa
-//   const getIds = () => {
-//     let array = []
-//     for(let x = 0; x < professionalBoard.length; x += 1){
-//         array.push(parseFloat(professionalBoard[x].id));
-//         if(array[x] === 8579-6){
-//             return array;
-//         }
-//     }
-//     return 'ID não identificada';
-//   }
-//   const searchEmployee = (id, detail) => {
-//     const Ids = getIds(id);
-//     return Ids
-//   };
-// console.log(searchEmployee(8579-6))
-//   module.exports = {searchEmployee};
-
-
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
+// Pesquisa
+const searchEmployee = (id, detail) => {
+  // Implemente seu código aqui
+  let empregado = '';
+  let array = '';
   for(let x = 0; x < professionalBoard.length; x += 1){
-    let array = []
-    array.push(professionalBoard[0].id);
-    console.log(array[x])
-    if(array[x] === 8579-6){
-        console.log(id + array[x])
-    }
-}
+    array = professionalBoard[x].id;
+    if(array === id){
+      empregado = professionalBoard[x];
+    };
+  };
+  if(!empregado){
+    throw new Error('ID não identificada');
+  };
+  if(!empregado[detail]){
+    return 'Informação indisponível';
+  };
+  return empregado[detail];
+};
+//   };
+console.log(searchEmployee('4678-2', 'firstName'));
+
+module.exports = {searchEmployee};
