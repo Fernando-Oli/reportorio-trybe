30fc7cf202f55d66eb3904fb59ed96efcd562364
Fernando Oliveira <primoflro@gmail.com>
2022-06-23 16:57:04 -0300

    Bloco 8

--- fundamentos/higher-order-functions-do-javascript/script.js
+++
@@ -1,9 +0,0 @@
-const wakeUp = () => 'Acordando!!';
-
-const makeCoffe = () => 'Bora tomar café!';
-
-const goToSleep = () => 'Partiu dormir!!';
-
-const doingThis = (action) => action();
-
-console.log(doingThis(wakeUp));
--- fundamentos/js-logica-de-programacao/04-03/bonus3-logica-de-programacao.js
+++ fundamentos/js-logica-de-programacao/04-03/bonus3-logica-de-programacao.js
@@ -1,15 +1,15 @@
-let asterisco = "*";
+let asterisco = '#';
+let resultado = '';
 let n = 5;
-let resultado = "";
-let inputPosition=n
+let inputPosition = n;
 for(let linha =1; linha <= n; linha++){
-    for(index=1;index<=n;index+=1){
-        if(index < inputPosition){
-            resultado = resultado + ' '
-        } else{
-            resultado = resultado + asterisco
+    for (index = 1;index <= n;index += 1) {
+        if (index < inputPosition) {
+            resultado += ' '
+        } else {
+            resultado += asterisco
         }}    
-  console.log(resultado)
-  resultado = ""
-  inputPosition-=1  
+  console.log(resultado);
+  resultado = '';
+  inputPosition -= 1;
 };
